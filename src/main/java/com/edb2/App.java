48fb4122a612839e961d6ec0fdc8dc8b2ab2786b
Lucas Nogueira <lucas.nogueira.072@ufrn.edu.br>
2022-10-25 09:23:26 -0300

    Initial commit

---
+++ src/main/java/com/edb2/App.java
@@ -0,0 +1,11 @@
+package com.edb2;
+
+public class App {
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
